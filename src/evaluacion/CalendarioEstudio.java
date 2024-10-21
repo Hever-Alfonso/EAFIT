1ac4a9ada17cb6478f1a667538dae4a9a80751f8
cortes1443 <jdcortesg2@eafit.edu.co>
2024-10-21 10:48:54 -0500

    Creada clase CalendarioEstudio
    Cree los atributos
    los metodos setters y getters
    y el constructor

--- src/evaluacion/CalendarioEstudio.java
+++ src/evaluacion/CalendarioEstudio.java
@@ -1,5 +1,30 @@
 package evaluacion;
 
 public class CalendarioEstudio {
+    private String nombre;
+    private String areaEstudio;
 
+    //Constructor
+    public CalendarioEstudio(String nombre, String areaEstudio){
+        this.nombre = nombre;
+        this.areaEstudio = areaEstudio;
+    }
+
+    //setters
+    private void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    private void setAreaEstudio(String areaEstudio){
+        this.areaEstudio = areaEstudio;
+    }
+
+    //getters
+    private String getNombre(){
+        return this.nombre;
+    }
+
+    private String getAreaEstudio(){
+        return this.areaEstudio;
+    }
 }

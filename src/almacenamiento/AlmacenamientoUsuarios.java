a1d3a7bb994479158cef7ef5e7a06429e0a76016
cortes1443 <jdcortesg2@eafit.edu.co>
2024-10-23 17:23:51 -0500

    Lector de almacenamiento
    Se ha añadido un metodo booleano para verificar si los datos de inicio de sesion de un usuario estan almacenados en el archivo .txt

--- src/almacenamiento/AlmacenamientoUsuarios.java
+++ src/almacenamiento/AlmacenamientoUsuarios.java
@@ -1,5 +1,7 @@
 package almacenamiento;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -10,6 +12,7 @@ import usuarios.Estudiante;
 public class AlmacenamientoUsuarios {
     FileWriter archivo;
     PrintWriter escritor;
+
 
 
     public void guardarUsuarioInfo(String email, String contraseña, String rol) throws IOException{
@@ -25,6 +28,28 @@ public class AlmacenamientoUsuarios {
             System.out.println("Error: " + e);
         } finally{
             archivo.close();
+        }
+    }
+
+    public boolean buscarUsuarioInfo(String email, String contraseña){
+        try (BufferedReader lector = new BufferedReader(new FileReader("usuarios.txt"))){
+
+            
+            String renglon = "";
+
+            while ((renglon= lector.readLine()) != null) { 
+
+                String[] bloques = renglon.split(",");
+
+                if(email.equals(bloques[0]) && contraseña.equals(bloques[1])){
+                    return true;
+                } 
+            }
+                return false;
+        } catch (IOException e) {
+            System.out.println("Error: " + e);
+
+            return false;
         }
     }
     
